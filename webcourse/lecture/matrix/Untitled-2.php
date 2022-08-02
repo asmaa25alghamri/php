7bdc508f24b481574e54480635e3649a0afa5770
asmaa25alghamri <74050641+asmaa25alghamri@users.noreply.github.com>
2022-08-02 09:32:29 +0300

    Delete webcourse/lecture directory

--- webcourse/lecture/matrix/Untitled-2.php
+++
@@ -1,4 +0,0 @@
-<?php
-$names = array("asmaa", "alaa", "tahany");
-echo  $names[0] . ", " . $names[1] . " and " . $names[2] . ".";
-?>
--- webcourse/lecture/matrix/Untitled-4.php
+++
@@ -1,7 +0,0 @@
-<?php
-$ages = array("15", "45", "20");
-sort($ages);
-rsort($ages);
-asort($ages);
-ksort($ages);
-?>
